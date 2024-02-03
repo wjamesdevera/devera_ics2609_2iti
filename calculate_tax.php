afc1a167246757316038fb7b13542e4bc9d0d40e
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-02-04 02:14:18 +0800

    display result styling

--- calculate_tax.php
+++ calculate_tax.php
@@ -1,12 +1,37 @@
 <?php
+session_start();
+
+require_once(dirname(__FILE__) ."/TaxCalculator.php");
 
 if (isPostRequest()) {
     $monthlySalary = sanitizeInput($_POST["monthly_salary"]);
     $isBiMonthly = isBiMonthly(sanitizeInput($_POST["bi_monthly"]));
+
+    $taxCalculator = new TaxCalculator($monthlySalary, $isBiMonthly);
+
+    $_SESSION['result'] = array(
+        'monthly_salary' => $taxCalculator->getMonthlySalary(),
+        'annual_salary' => $taxCalculator->getAnnualSalary(),
+        'annual_tax' => $taxCalculator->getAnnualTax(),
+        'monthly_tax' => $taxCalculator->getMonthlyTax(),
+    );
+    header("location: index.php");
+    die();
 } else {
     header("location: index.php");
     die();
 }
+?>
+<?= 'Monthly Salary: ' . $taxCalculator->getMonthlySalary() ?>
+<br>
+<?= 'Annual Salary: ' . $taxCalculator->getAnnualSalary() ?>
+<br>
+<?= 'Est. Annual Tax: ' . $taxCalculator->getAnnualTax() ?>
+<br>
+<?= 'Est. Monthly Tax: ' . $taxCalculator->getMonthlyTax() ?>
+<br>
+
+<?php
 
 function isPostRequest(): bool
 {
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+session_start();
+
+?>
 <!doctype html>
 <html lang="en">
 
@@ -12,35 +17,62 @@
     <header>
     </header>
     <main class="">
-        <form action="calculate_tax.php" method="POST">
-            <div class="form-title">
-                <h2>Taxxy: Tax Calculator.</h2>
-                <p>Enter salary to calculate taxes instantly!</p>
-            </div>
-            <div class="form-input">
-                <input type="number" name="monthly_salary" placeholder="Monthly Salary" autocomplete="off" autocapitalize="off">
-            </div>
-            <fieldset>
-                <legend>
-                    <p>Type</p>
-                </legend>
-                <div class="input-radio form-input">
-                    <label for="monthly" class="radio">
-                        <input type="radio" name="bi_monthly" id="monthly" value="monthly">
-                        <div class="radio__radio"></div>
-                        Monthly
-                    </label>
-                    <label for="bi-monthly" class="radio">
-                        <input type="radio" name="bi_monthly" id="bi-monthly" value="bi-monthly">
-                        <div class="radio__radio"></div>
-                        Bi-Monthly
-                    </label>
+        <?php if (isset($_SESSION['result'])) : ?>
+            <?php
+            require_once 'TaxCalculator.php';
+            $result = $_SESSION["result"];
+            ?>
+            <div class="result-card">
+                <h2>Tax Computation:</h2>
+                <div class="">
+                    <p>Monthly Salary:
+                        <?= $result['monthly_salary'] ?>
+                    </p>
+                    <p>
+                        Annual Salary:
+                        <?= $result['annual_salary'] ?>
+                    </p>
+                    <p>
+                        Estimated Annual Tax:
+                        <?= $result['annual_tax'] ?>
+                    </p>
+                    <p>
+                        Estimated Monthly Tax:
+                        <?= $result['monthly_tax'] ?>
+                    </p>
                 </div>
-            </fieldset>
-            <div class="form-submit">
-                <input type="submit" value="Submit">
             </div>
-        </form>
+        <?php else : ?>
+            <form action="calculate_tax.php" method="POST">
+                <div class="form-title">
+                    <h2>Taxxy: Tax Calculator.</h2>
+                    <p>Enter salary to calculate taxes instantly!</p>
+                </div>
+                <div class="form-input">
+                    <input type="number" name="monthly_salary" placeholder="Monthly Salary" autocomplete="off" autocapitalize="off" required>
+                </div>
+                <fieldset>
+                    <legend>
+                        <p>Type</p>
+                    </legend>
+                    <div class="input-radio form-input">
+                        <label for="monthly" class="radio">
+                            <input type="radio" name="bi_monthly" id="monthly" value="monthly" checked>
+                            <div class="radio__radio"></div>
+                            Monthly
+                        </label>
+                        <label for="bi-monthly" class="radio">
+                            <input type="radio" name="bi_monthly" id="bi-monthly" value="bi-monthly">
+                            <div class="radio__radio"></div>
+                            Bi-Monthly
+                        </label>
+                    </div>
+                </fieldset>
+                <div class="form-submit">
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
+        <?php endif ?>
     </main>
     <footer>
         <small>Copyright &copy; <?= date('Y') ?> De Vera</small>
