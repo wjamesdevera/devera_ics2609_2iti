92421aab73577ec9a45ac9d7f70104d7615a796a
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-02-16 23:16:34 +0800

    change result summary styles

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <!doctype html>
-<html lang="en" data-bs-theme="dark">
+<html lang="en" data-bs-theme="light">
 
 <head>
     <meta charset="utf-8">
@@ -25,40 +25,44 @@ session_start();
         </nav>
     </header>
     <main class="container">
-        <div class="d-flex justify-content-center align-items-center mb-5 ">
+        <div class="d-flex justify-content-center align-items-center mb-5">
             <?php if (isset($_SESSION['result'])) : ?>
                 <?php
                 require_once 'TaxCalculator.php';
                 $result = $_SESSION["result"];
                 ?>
-
-                <div class="result-card border border-light-subtle p-4 rounded">
-                    <h2>Tax Computation:</h2>
-                    <div class="">
-                        <p>Monthly Salary:
-                            <?= '&#8369; ' . $result['monthly_salary'] ?>
-                        </p>
-                        <p>
-                            Annual Salary:
-                            <?= '&#8369; ' . $result['annual_salary'] ?>
-                        </p>
-                        <p>
-                            Estimated Annual Tax:
-                            <?= '&#8369; ' . $result['annual_tax'] ?>
-                        </p>
-                        </p>
-                        <p>
-                            Estimated Monthly Tax:
-                            <?= '&#8369; ' . $result['monthly_tax'] ?>
-                        </p>
+                    <div class="border rounded p-4">
+                        <table class="table table-borderless">
+                            <thead>
+                                <tr class="">
+                                    <th class="col" colspan="3">Income Tax Calculation</th>
+                                </tr>
+                                <tr>
+                                    <th class="col"></th>
+                                    <th class="col">Monthly</th>
+                                    <th class="col">Annually</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                <tr>
+                                    <th class="col">Salary</th>
+                                    <td class="col"><?= '&#8369; ' . $result['monthly_salary'] ?></td>
+                                    <td class="col"><?= '&#8369; ' . $result['annual_salary'] ?></td>
+                                </tr>
+                                <tr class="table-success">
+                                    <th class="col">Estimated Tax</th>
+                                    <td class="col"><?= '&#8369; ' . $result['monthly_tax'] ?></td>
+                                    <td class="col"><?= '&#8369; ' . $result['annual_tax'] ?></td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
-                </div>
-                <?php unset($_SESSION["result"]) ?>
+                <!-- <?php // unset($_SESSION["result"]) ?> -->
             <?php else : ?>
                 <form action="calculate_tax.php" method="POST" class="tax-calculator-form border border-light-subtle p-4 rounded">
                     <div class="">
                         <h2>Tax Calculator</h2>
-                        <p>Enter salary to calculate taxes instantly!</p>
+                        <p>Enter monthly salary to calculate taxes instantly!</p>
                     </div>
                     <div class="input-group mb-3">
                         <span class="input-group-text">&#8369;</span>
@@ -96,7 +100,7 @@ session_start();
                         <th class="col">TAX DUE</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody class="table-group-divider">
                     <tr>
                         <td class="col">Not over PHP250,000.00</td>
                         <td class="col">Extempted from personal income tax</td>
@@ -128,6 +132,8 @@ session_start();
     <footer class="container mt-auto p-2 text-center">
         <small>Copyright &copy; <?= date('Y') ?> De Vera</small>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
-
 </html>
